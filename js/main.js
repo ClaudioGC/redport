b27d9163f6bf194d6aaec228ff2b436b9e6990d3
ClaudioGC <gomezcorralclaudio@gmail.com>
2016-07-11 00:48:14 -0400

    Cambio de estilos gráficos
    cambio de paleta de colores
    Ajuste tipográfico
    Ajuste de estilo en botones

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 
+//despliegue de las opciones "indica el grado de gravedad"
 $('.toggle_button_big').on('click',function(){
 	$('.form_content').slideToggle('slow');
 });
 
+//añadir o quitar personas a la pregunta "¿están bien?"
 $('#toggle_small_4').on('click',function(){
 
 	var clone = $('#name_list').clone();
-	$('.lista_form_1').before(clone);
+	$('.lista_form_1').after(clone);
+	$('#lista_form_1').appendChild('#name_list');
 });
 
