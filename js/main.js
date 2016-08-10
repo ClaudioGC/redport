63c9ee75c14b5657dc0e2cabf52de9b0a742578a
ClaudioGC <gomezcorralclaudio@gmail.com>
2016-08-10 14:46:19 -0400

    js

--- js/main.js
+++ js/main.js
@@ -5,9 +5,9 @@ $('.toggle_button_big').on('click',function(){
 });
 
 //añadir o quitar personas a la pregunta "¿están bien?"
-$('#toggle_small_4').on('click',function(){
+//$('#toggle_small_4').on('click',function(){
 
-	var clone = $('#name_list').clone();
-	$('.lista_form_1').after(clone);
-	$('#lista_form_1').appendChild('#name_list');
-});
+	//var clone = $('#name_list').clone();
+	//$('.lista_form_1').after(clone);
+	//$('#lista_form_1').appendChild('#name_list');
+//});
